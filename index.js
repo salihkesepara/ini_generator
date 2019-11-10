668cb00b3f37a2aae7e3674790ef522eaf9086d3
Salih Kesepara <salihkesepara@gmail.com>
2019-11-10 23:36:37 +0300

    some variables are changed.

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const messages = require('./constants/messages');
 const inputs = './inputs';
 
 let inputFile;
+let outputFile;
 let inputFileName;
 let outputFileName;
 let params;
+let rl;
+let fileRoot;
+let regExp;
 
 require.extensions['.ini'] = function (module, filename) {
   module.exports = fs.readFileSync(filename, 'utf8');
@@ -18,20 +22,20 @@ fs.readdirSync(inputs).forEach(file => {
 });
 console.groupEnd();
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 function generateFile() {
   // Get file
-  let outputFile = inputFile;
+  outputFile = inputFile;
 
   // Generate file
   params.forEach((item) => {
-    const regExp = new RegExp(`${item.id}`, 'g');
+    regExp = new RegExp(`${item.id}`, 'g');
     outputFile = outputFile.replace(regExp, item.value);
   });
 
   // Input file root name
-  const fileRoot = `./outputs/${outputFileName}.ini`;
+  fileRoot = `./outputs/${outputFileName}.ini`;
 
   // Write file in output folder
   fs.writeFile(fileRoot, outputFile, (err) => {
